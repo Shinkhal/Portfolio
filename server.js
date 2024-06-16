57b5c243679ee442748974cc808b67ae84d4b2f5
Shinkhal Sinha <shinkhalsinha@gmail.com>
2024-06-16 17:54:24 +0530

    Updated server.js and added .env file

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-// backend/server.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
+const axios = require('axios'); 
 const dotenv = require('dotenv');
 
-dotenv.config(); // Load environment variables from .env file
-
+dotenv.config();
 const app = express();
+const RECAPTCHA_SECRET_KEY = process.env.SECRET_KEY;
 const port = process.env.PORT || 4000;
 
-// Middleware
 app.use(bodyParser.json());
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
-  }));
 
-// Nodemailer configuration
+app.use(cors({
+    origin: "http://localhost:3000", 
+    credentials: true
+}));
+
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
@@ -26,7 +25,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
   },
-  debug: false // Disable debug in production
+  debug: false
 });
 
 app.post('/send-email', (req, res) => {
@@ -34,7 +33,7 @@ app.post('/send-email', (req, res) => {
 
   const mailOptionsOwner = {
     from: email,
-    to: process.env.MY_EMAIL, // Your email id
+    to: process.env.MY_EMAIL,
     subject: 'New Contact Form Submission on your portfolio',
     text: `You have a new message from:
     Name: ${name}
@@ -67,6 +66,35 @@ app.post('/send-email', (req, res) => {
       res.status(200).json({ message: 'Emails sent successfully!' });
     });
   });
+});
+
+app.post('/verify-recaptcha', async (req, res) => {
+  const { token } = req.body;
+
+  if (!token) {
+    return res.status(400).json({ success: false, error: 'reCAPTCHA token is missing' });
+  }
+
+  try {
+    const response = await axios.post(
+      `https://www.google.com/recaptcha/api/siteverify`,
+      null,
+      {
+        params: {
+          secret: RECAPTCHA_SECRET_KEY,
+          response: token,
+        },
+      }
+    );
+
+    if (response.data.success) {
+      res.json({ success: true, message:"Verification done" });
+    } else {
+      res.status(400).json({ success: false, error: response.data['error-codes'] });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
 });
 
 app.listen(port, () => {
